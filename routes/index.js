ecc0e8a260f79d9567382ed07e823fd6c06b1468
Malak371 <113465527+Malak371@users.noreply.github.com>
2024-12-15 18:08:51 -0500

    commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import userRoutes from './userRoutes.js';
+import attendeePostsRoutes from './attendeePostsRoutes.js';
+import organizerPostsRoutes from './organizerPostsRoutes.js';
+
+const router = express.Router();
+const __dirname = path.resolve();
+
+router.use('/', userRoutes);
+//router.use('/posts', attendeePostsRoutes);
+//router.use('/events', organizerPostsRoutes);
+//router.use('/create', organizerPostsRoutes);
+router.use('/', attendeePostsRoutes);
+router.use('/', organizerPostsRoutes);
+
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static/homePage.html'));
+});
+
+export default router;
